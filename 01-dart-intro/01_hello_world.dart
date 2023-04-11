629d6551eee211528b0a6bbf82653569b4de50c2
Osvaldo Valentin <69062379+ValentinOsvaldo@users.noreply.github.com>
2023-04-10 19:00:38 -0600

    first commit

---
+++ 01-dart-intro/01_hello_world.dart
@@ -0,0 +1,11 @@
+void main() {
+  /**
+   * Comment multiline
+   */
+  // String myName = 'Osvaldo';
+  // late final myName;
+  const myName = 'Osvaldo';
+
+  print('Hello $myName');
+  print('Hello ${myName.toLowerCase()}');
+}
