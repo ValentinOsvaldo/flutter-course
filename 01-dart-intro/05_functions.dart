c95439b1603e6d5375933d836590dcc81970361d
Osvaldo Valentin <69062379+ValentinOsvaldo@users.noreply.github.com>
2023-04-12 14:06:28 -0600

    functions params with name

--- 01-dart-intro/05_functions.dart
+++ 01-dart-intro/05_functions.dart
@@ -3,6 +3,8 @@ void main() {
 
   print(addTwoNumbers(1, 1));
   print(addTwoNumbersOptional(2));
+  
+  print( greetPerson( name: 'Osvaldo', message: 'Que onda' ) );
 }
 
 String greetEveryone() => 'Hello everyone!';
@@ -13,4 +15,8 @@ int addTwoNumbersOptional(int a, [int b = 1]) {
   // b ??= 1;
 
   return a + b;
+}
+
+String greetPerson({ required String name, String? message = 'Hola' }) {
+  return '$message, $name';
 }
