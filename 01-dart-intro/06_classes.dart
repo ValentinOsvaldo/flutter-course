5faf1f51a482d4920fadeeba7acc90889c3b681a
Osvaldo Valentin <69062379+ValentinOsvaldo@users.noreply.github.com>
2023-04-12 14:47:32 -0600

    @override

--- 01-dart-intro/06_classes.dart
+++ 01-dart-intro/06_classes.dart
@@ -1,5 +1,5 @@
 void main() {
-  final Hero wolverine = Hero('Logan', 'Regeneración');
+  final Hero wolverine = Hero(name: 'Logan', power: 'Regeneración');
 
   print(wolverine);
   print(wolverine.name);
@@ -10,9 +10,14 @@ class Hero {
   String? name;
   String? power;
 
-  Hero(this.name, this.power);
+  Hero({required this.name, this.power = 'Sin Poder'});
 
   // Hero( String pName, String pPower )
   //  : name = pName,
   //    power = pPower;
+
+  @override
+  String toString() {
+    return '$name - $power';
+  }
 }
